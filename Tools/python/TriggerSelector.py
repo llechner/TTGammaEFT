78d9d9302de23afd4b41e0bb4c3a6f7a4528c8a3
Lukas Lechner <lukas.k.lechner@gmail.com>
2019-02-08 11:23:02 +0100

    bugfix missing trigger

--- Tools/python/TriggerSelector.py
+++ Tools/python/TriggerSelector.py
@@ -18,24 +18,20 @@ class TriggerSelector:
                 self.m  = [ "HLT_IsoMu24", "HLT_IsoTkMu24" ]
                 self.ee = [ "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ" ]
                 self.e  = [ "HLT_Ele27_WPTight_Gsf" ]
-                self.em = [ "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL", "HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL", "HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL"]
-#                self.em = [ "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL", "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL", "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ"]
+                self.em = [ "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL", "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL", "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ"]
 
         elif year == 2017:
             if singleLepton:
                 self.m  = [ "HLT_IsoMu24", "HLT_IsoMu27" ]
-                self.e  = [ "HLT_Ele32_WPTight_Gsf_L1DoubleEG"]
-#                if era not in [ "B", "C" ]: self.e += [ "HLT_Ele32_WPTight_Gsf" ]
+                self.e  = [ "HLT_Ele32_WPTight_Gsf_L1DoubleEG", "HLT_Ele32_WPTight_Gsf" ]
                 self.mm = None
                 self.em = None
                 self.ee = None
             else:
-                self.mm = [ "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ" ]
-#                if era != "B": self.mm += [ "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8" ]
+                self.mm = [ "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ", "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8" ]
                 self.m  = [ "HLT_IsoMu24", "HLT_IsoMu27" ]
                 self.ee = [ "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL" ]
-                self.e  = [ "HLT_Ele32_WPTight_Gsf_L1DoubleEG"]
-#                if era not in [ "B", "C" ]: self.e += [ "HLT_Ele32_WPTight_Gsf" ]
+                self.e  = [ "HLT_Ele32_WPTight_Gsf_L1DoubleEG", "HLT_Ele32_WPTight_Gsf" ]
                 self.em = [ "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ" ]
 
         elif year == 2018:
@@ -56,16 +52,19 @@ class TriggerSelector:
             raise NotImplementedError( "Trigger selection %r not implemented" %year )
 
         # define which triggers should be used for which dataset
-        self.DoubleMuon     = "(%s)"%"||".join( self.mm ) if self.mm else None
-        self.DoubleEG       = "(%s)"%"||".join( self.ee ) if self.ee else None
-        self.EGamma         = "(%s)"%"||".join( self.ee ) if self.ee else None
-        self.MuonEG         = "(%s)"%"||".join( self.em ) if self.em else None
-        self.SingleMuon     = "(%s)"%"||".join( self.m  ) if self.m  else None
-        self.SingleElectron = "(%s)"%"||".join( self.e  ) if self.e  else None
+        self.DoubleMuon     = "(%s)"%"||".join( ["Alt$(%s,0)"%trigger for trigger in self.mm] ) if self.mm else None
+        self.DoubleEG       = "(%s)"%"||".join( ["Alt$(%s,0)"%trigger for trigger in self.ee] ) if self.ee else None
+        eGamma = []
+        if self.e:  eGamma += ["Alt$(%s,0)"%trigger for trigger in self.e]
+        if self.ee: eGamma += ["Alt$(%s,0)"%trigger for trigger in self.ee]
+        self.EGamma         = "(%s)"%"||".join( eGamma  ) if eGamma  else None
+        self.MuonEG         = "(%s)"%"||".join( ["Alt$(%s,0)"%trigger for trigger in self.em] ) if self.em else None
+        self.SingleMuon     = "(%s)"%"||".join( ["Alt$(%s,0)"%trigger for trigger in self.m]  ) if self.m  else None
+        self.SingleElectron = "(%s)"%"||".join( ["Alt$(%s,0)"%trigger for trigger in self.e]  ) if self.e  else None
 
         # define an arbitrary hierarchy
         if year == 2018:
-            self.PDHierarchy = [ "DoubleMuon", "EGamma", "MuonEG", "SingleMuon", "SingleElectron" ]
+            self.PDHierarchy = [ "DoubleMuon", "EGamma", "MuonEG", "SingleMuon" ]
         else:
             self.PDHierarchy = [ "DoubleMuon", "DoubleEG", "MuonEG", "SingleMuon", "SingleElectron" ]
 
