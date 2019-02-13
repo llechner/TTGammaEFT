ff25afcd3c050b888ec0821912cfd2063b0bfda0
Lukas Lechner <lukas.k.lechner@gmail.com>
2019-02-13 13:19:42 +0100

    cleanup

---
+++ Analysis/python/run/cache_nll_incl.sh
@@ -0,0 +1,34 @@
+#ctZ
+
+#Dileps
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2016 --variables ctZ ctZI --cores 6 --selections 2l
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2017 --variables ctZ ctZI --cores 6 --selections 2l
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2018 --variables ctZ ctZI --cores 6 --selections 2l
+#SemiLep
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2016 --variables ctZ ctZI --cores 6 --selections 1l
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2017 --variables ctZ ctZI --cores 6 --selections 1l
+python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2018 --variables ctZ ctZI --cores 6 --selections 1l
+#Combined
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2016 2017 2018 --variables ctZ ctZI --cores 6 --selections 2l
+#SemiLep Combined
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2016 2017 2018 --variables ctZ ctZI --cores 6 --selections 1l
+#All Combined
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2016 2017 2018 --variables ctZ ctZI --cores 6 --selections 1l 2l
+
+exit
+#ctW
+
+#Dilep
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2016 --variables ctW ctWI --cores 6 --selections 2l
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2017 --variables ctW ctWI --cores 6 --selections 2l
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2018 --variables ctW ctWI --cores 6 --selections 2l
+#SemiLep
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2016 --variables ctW ctWI --cores 6 --selections 1l
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2017 --variables ctW ctWI --cores 6 --selections 1l
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2018 --variables ctW ctWI --cores 6 --selections 1l
+#Combined
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2016 2017 2018 --variables ctW ctWI --cores 6 --selections 2l
+#SemiLep Combined
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2016 2017 2018 --variables ctW ctWI --cores 6 --selections 1l
+#All Combined
+#python cache_nll.py $1 $2 --inclusive --binning 50 -2 2 50 -2 2 --years 2016 2017 2018 --variables ctW ctWI --cores 6 --selections 1l 2l
