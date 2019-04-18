6399a166c748acfa9750f825b07f9fa49085732a
Lukas Lechner <lukas.k.lechner@gmail.com>
2019-04-18 11:40:57 +0200

    adding private data sample scripts

---
+++ postprocessing/nanoPostProcessing_Run2017_private_semilep.sh
@@ -0,0 +1,29 @@
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample DoubleMuon_Run2017B_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample DoubleMuon_Run2017C_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample DoubleMuon_Run2017D_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample DoubleMuon_Run2017E_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample DoubleMuon_Run2017F_31Mar2018 # SPLIT10
+#
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample DoubleEG_Run2017B_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample DoubleEG_Run2017C_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample DoubleEG_Run2017D_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample DoubleEG_Run2017E_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample DoubleEG_Run2017F_31Mar2018 # SPLIT10
+#
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample MuonEG_Run2017B_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample MuonEG_Run2017C_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample MuonEG_Run2017D_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample MuonEG_Run2017E_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample MuonEG_Run2017F_31Mar2018 # SPLIT10
+#
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample SingleMuon_Run2017B_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample SingleMuon_Run2017C_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample SingleMuon_Run2017D_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample SingleMuon_Run2017E_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample SingleMuon_Run2017F_31Mar2018 # SPLIT10
+#
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample SingleElectron_Run2017B_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample SingleElectron_Run2017C_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample SingleElectron_Run2017D_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample SingleElectron_Run2017E_31Mar2018 # SPLIT10
+python nanoPostProcessing.py --triggerSelection --writeToDPM --skim semilep --year 2017 --processingEra TTGammaEFT_PP_2017_TTG_Data_v14 --fileBasedSplitting --sample SingleElectron_Run2017F_31Mar2018 # SPLIT10
