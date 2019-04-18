9bfce3f201f301e7cd30fed78c3b29eea70aaab0
Lukas Lechner <lukas.k.lechner@gmail.com>
2019-04-18 10:35:27 +0200

    gen sample comparison update

---
+++ plots/plotsLukas/gen/ttgammaComparison.sh
@@ -0,0 +1,13 @@
+#python ttgammaComparison.py --version v6 --plot_directory runCardComparison --selection dilepOS-offZll-offZSFllg-nPhoton1p-pTG20-mll20 &
+
+#python ttgammaComparison.py --version v6 --plot_directory runCardComparison --selection dilepOS-offZll-offZSFllg-nJet2p-nBTag1p-nPhoton1p-pTG20-mll20 &
+python ttgammaComparison.py --version v6 --plot_directory runCardComparison --selection nLep1-nJet4p-nBTag1p-nPhoton1p-pTG20 &
+#python ttgammaComparison.py --version v6 --plot_directory runCardComparison --selection nLep1-nJet4p-nBTag2p-nPhoton1p-pTG20 &
+
+#python ttgammaComparison.py --version v6 --plot_directory runCardComparison --selection dilepOS-offZll-nJet2p-nBTag1p-nPhoton0-mll20 &
+#python ttgammaComparison.py --version v6 --plot_directory runCardComparison --selection nLep1-nJet4p-nBTag1p-nPhoton0 &
+
+#python ttgammaComparison.py --version v6 --plot_directory runCardComparison --selection dilepOS-offZll-nJet2p-nBTag1p-mll20 &
+#python ttgammaComparison.py --version v6 --plot_directory runCardComparison --selection dilepOS-offZll-mll20 &
+
+#python ttgammaComparison.py --version v6 --plot_directory runCardComparison --selection dilepOS-offZll-offZSFllg-nJet2p-nBTag2p-nPhoton1p-pTG20-mll20 &
