92d6f1dda12fc7df1e7f5bac0b005879ffe46e2f
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2019-05-21 09:49:30 +0200

    user

--- Tools/python/user.py
+++ Tools/python/user.py
@@ -5,23 +5,20 @@ redirector_global = 'root://cms-xrd-global.cern.ch/'
 
 if os.environ['USER'] in ['schoef', 'rschoefbeck', 'schoefbeck']:
     results_directory               = "/afs/hephy.at/data/rschoefbeck02/TTGammaEFT/results/"
-    skim_output_directory           = "/afs/hephy.at/data/rschoefbeck02/TTGammaEFT/skims/"
-    skim_directory                  = "/afs/hephy.at/data/dspitzbart01/TTGammaEFT/skims/"
     tmp_directory                   = "/afs/hephy.at/data/rschoefbeck02/TTGammaEFT_tmp/"
     plot_directory                  = "/afs/hephy.at/user/r/rschoefbeck/www/TTGammaEFT/"
-    data_directory                  = "/afs/hephy.at/data/rschoefbeck01/cmgTuples/"
     postprocessing_directory        = "TTGammaEFT_PP_2016_TTG_v5/inclusive/"
     postprocessing_output_directory = "/afs/hephy.at/data/rschoefbeck02/cmgTuples/"
     analysis_results                = results_directory
 
     cache_directory                     = "/afs/hephy.at/data/rschoefbeck01/TTGammaEFT/cache/"
 
+    mva_directory                       = "/afs/hephy.at/data/llechner01/TTGammaEFT/mva/"
+    dpm_directory                       = '/dpm/oeaw.ac.at/home/cms/store/user/llechner/'
+
 if os.environ['USER'] in ['llechner']:
     tmp_directory                       = "/afs/hephy.at/data/llechner01/Top_tmp/"
     results_directory                   = "/afs/hephy.at/data/llechner01/TTGammaEFT/results/"
-
-#    skim_directory                      = "/afs/hephy.at/data/llechner01/TTGammaEFT/skims/"
-#    skim_output_directory               = "/afs/hephy.at/data/llechner01/TTGammaEFT/skims/"
 
     mva_directory                       = "/afs/hephy.at/data/llechner01/TTGammaEFT/mva/"
 
