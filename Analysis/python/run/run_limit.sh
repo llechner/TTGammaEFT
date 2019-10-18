817969229cbf3a6b8a7a3717dd94f4609c554f25
Lukas Lechner <lukas.k.lechner@gmail.com>
2019-10-18 17:32:52 +0200

    update fit script

--- Analysis/python/run/run_limit.sh
+++ Analysis/python/run/run_limit.sh
@@ -3,11 +3,47 @@
 overwrite="--overwrite"
 #overwrite=""
 
-regions="TT3 TT4p WJets3 WJets4p DY3 DY4p misTT2 misDY3 misDY4p VG3 VG4p SR3onM3 SR4ponM3 SR3offM3 SR4poffM3 SR3lowIso SR4plowIso SR3highIso SR4phighIso"
+regions="SR3M3 SR3Iso"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions}"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addDYSF"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addMisIDSF"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addDYSF --addMisIDSF"
+
+regions="SR4pM3 SR4pIso"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions}"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addDYSF"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addMisIDSF"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addDYSF --addMisIDSF"
+
+regions="TT3 TT4p WJets3 WJets4p DY3 DY4p misTT2 misDY3 misDY4p VG3 VG4p SR3M3 SR4pM3 SR3Iso SR4pIso"
 runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions}"
 runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addDYSF"
 runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addMisIDSF"
 runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addDYSF --addMisIDSF"
+
+regions="TT3 TT4p WJets3 WJets4p DY3 DY4p misTT2 misDY3 misDY4p VG3 VG4p SR3Iso SR4pIso"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions}"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addDYSF"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addMisIDSF"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addDYSF --addMisIDSF"
+
+regions="TT3 TT4p WJets3 WJets4p DY3 DY4p misTT2 misDY3 misDY4p VG3 VG4p SR3lowIso SR4plowIso SR3highIso SR4phighIso SR3M3 SR4pM3 SR3Iso SR4pIso"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions}"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addDYSF"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addMisIDSF"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addDYSF --addMisIDSF"
+
+regions="DY2 DY3 DY4 DY5 DY4p misDY2 misDY3 misDY4 misDY5 misDY4p VG2 VG3 VG4 VG5 VG4p"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions}"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addDYSF"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addMisIDSF"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addDYSF --addMisIDSF"
+
+regions="TT3 TT4p WJets3 WJets4p DY3 DY4p misTT2 misDY3 misDY4p VG3 VG4p SR3onM3 SR4ponM3 SR3offM3 SR4poffM3 SR3lowIso SR4plowIso SR3highIso SR4phighIso SR3 SR4p"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions}"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addDYSF"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addMisIDSF"
+#runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions} --addDYSF --addMisIDSF"
 
 regions="TT3 TT4p WJets3 WJets4p DY3 DY4p misTT2 misDY3 misDY4p VG3 VG4p SR3onM3 SR4ponM3 SR3offM3 SR4poffM3 SR4phighIso"
 #runNotifier.sh "python run_limit.py $@ ${overwrite} --useRegions ${regions}"
