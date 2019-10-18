f0dffc8096471d3c76772eea24a9f9e8dd2078fc
Lukas Lechner <lukas.k.lechner@gmail.com>
2019-10-18 17:33:15 +0200

    cleanup

--- Analysis/python/run/allEstimates.sh
+++
@@ -1,4 +0,0 @@
-runNotifier.sh "python run_estimate.py --overwrite --cores 1 --selectRegion 0 --controlRegion VG3 --selectEstimator Data --year 2016"
-runNotifier.sh "python run_estimate.py --overwrite --cores 1 --selectRegion 1 --controlRegion VG3 --selectEstimator Data --year 2016"
-runNotifier.sh "python run_estimate.py --overwrite --cores 1 --selectRegion 2 --controlRegion VG3 --selectEstimator Data --year 2016"
-runNotifier.sh "python run_estimate.py --overwrite --cores 1 --selectRegion 3 --controlRegion VG3 --selectEstimator Data --year 2016"
